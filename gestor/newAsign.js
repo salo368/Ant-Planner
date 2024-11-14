fe16191a608699e5901a1b2b50422e7abb5b7ccd
sebastianBarandica <sebastianbarandica@uninorte.edu.co>
2024-11-13 21:40:49 -0500

    tabla asing /bug popout

---
+++ gestor/newAsign.js
@@ -0,0 +1,18 @@
+function newUserAsign() {
+    console.log("newUserAsign");
+    const modal = document.getElementById("add-new-user-form");
+    if (modal.style.display === "flex") {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto"; 
+    } else {
+        modal.style.display = "flex";
+        document.body.style.overflow = "hidden"; 
+    }
+}
+
+document.getElementById("add-new-user-form").addEventListener("click", function(event) {
+    const modalContent = document.querySelector(".add-new-user-asign-table");
+    if (!modalContent.contains(event.target)) {
+        newUserAsign();
+    }
+});
